14b12ce8dc8fe4089d73f17174d30cbe878b286d
just-in-tsui <justintc2002@gmail.com>
2022-09-17 11:23:01 +0800

    test calendar part 1

---
+++ codeitsuisse/routes/calendarDays.py
@@ -0,0 +1,105 @@
+import logging
+import json
+import calendar, datetime
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+def checkLeapYear(Year):
+    if (Year % 400 == 0) and (Year % 100 == 0):
+        return 366
+    # not divided by 100 means not a century year
+    # year divided by 4 is a leap year
+    elif (Year % 4 == 0) and (Year % 100 != 0):
+        return 366
+    # if not divided by both 400 (century year) and 4 (not century year)
+    # year is not leap year
+    else:
+        return 365
+
+
+@app.route('/calendarDays', methods = ['POST'])
+def evaluate():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    inputValue = data.get("numbers")
+    year = inputValue[0]
+    maxDays = checkLeapYear(year)  # check leap year
+    days = inputValue[1:]
+    days.sort()
+    state = ""
+    for i in range(96):
+        if i % 8 == 7:
+            state += ","
+        else:
+            state += " "
+    weekends = []
+    weekdays = []
+    new_days = []
+    for i in range(12):
+        weekdays.append([])
+        weekends.append([])
+        new_days.append([])
+
+    for day in days:
+        date = datetime.date(year, 1, 1)  # Will give 1996-01-01
+        delta = datetime.timedelta(day - 1)  # str(delta) will be '31 days, 0:00:00'
+        newdate = date + delta  # date = 2022-03-21
+        # print(newdate)
+        month_pos = newdate.month - 1
+        new_days[month_pos].append(day)
+    state = list(state)
+    for day in days:
+        date = datetime.date(year, 1, 1)  # Will give 1996-01-01
+        delta = datetime.timedelta(day - 1)  # str(delta) will be '31 days, 0:00:00'
+        newdate = date + delta  # date = 2022-03-21
+        # print(newdate)
+        month_pos = newdate.month - 1  # month = 3 -> [2]
+        dayOfWeek = newdate.weekday()  # Mon = 0 ,Sun = 6
+        week = ["m", "t", "w", "t", "f", "s", "s"]
+        # print(month_pos * 8 + dayOfWeek)
+
+        state[month_pos * 8 + dayOfWeek] = week[dayOfWeek]
+
+    for i in range(1, maxDays):
+        date = datetime.date(year, 1, 1)  # Will give 1996-01-01
+        delta = datetime.timedelta(i - 1)  # str(delta) will be '31 days, 0:00:00'
+        newdate = date + delta  # date = 2022-03-21
+        month_pos = newdate.month - 1  # month = 3 -> [2]
+        if newdate.weekday() == 5 or newdate.weekday() == 6:
+            weekends[month_pos].append(i)
+            weekends[month_pos].sort()
+        else:
+            weekdays[month_pos].append(i)
+            weekdays[month_pos].sort()
+
+    for i in range(12):
+        isWeekend = False
+        isWeekday = False
+        print(weekends[i], days)
+        text1 = ["w", "e", "e", "k", "e", "n", "d"]
+        text2 = ["w", "e", "e", "k", "d", "a", "y"]
+        text3 = ["a", "l", "l", "d", "a", "y", "s"]
+
+        if all(x in new_days[i] for x in weekends[i]):  # i = month -1
+            isWeekend = True
+        if all(x in new_days[i] for x in weekdays[i]):
+            isWeekday = True
+        if isWeekend:
+            for j in range(7):
+                state[8 * i + j] = text1[j]
+        if isWeekday:
+            for j in range(7):
+                state[8 * i + j] = text2[j]
+        if isWeekend and isWeekday:
+            for j in range(7):
+                state[8 * i + j] = text3[j]
+
+    print(state)
+
+    result = inputValue * inputValue
+    logging.info("My result :{}".format(result))
+    return json.dumps(result)
+
