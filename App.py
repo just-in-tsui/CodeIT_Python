e1a5dd8af52d64b6651725bdf989a99b3e0ca735
just-in-tsui <justintc2002@gmail.com>
2022-09-17 20:41:40 +0800

    quordle part1

--- App.py
+++ App.py
@@ -3,27 +3,13 @@ import socket
 from codeitsuisse import app
 import time
 
-import redis
+
 from flask import Flask
 logger = logging.getLogger(__name__)
 app.config["DEBUG"] = True
-cache = redis.Redis(host='redis', port=23946)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
 
 @app.route('/', methods=['GET','POST'])
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
 def default_route():
     return "Python Template"
 
